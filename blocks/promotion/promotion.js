34fb6afc79dfbed6ad4cc5669ad7de3fe5678757
Craig McLean <craig.mclean@ensemble.com>
2026-02-06 09:51:45 -0500

    promotion block

--- blocks/promotion/promotion.js
+++ blocks/promotion/promotion.js
@@ -4,9 +4,22 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
-  // setup image columns
   [...block.children].forEach((row) => {
-    [...row.children].forEach((col) => {
+    [...row.children].forEach((col, index) => {
+      const anchors = col.querySelectorAll('a');
+      anchors.forEach((anchor) => {
+        if (!anchor.querySelector('span')) {
+          const span = document.createElement('span');
+          while (anchor.firstChild) {
+            span.append(anchor.firstChild);
+          }
+          anchor.append(span);
+        }
+      });
+
+      if (index === 0) {
+        col.classList.add('text-container');
+      }
       const pic = col.querySelector('picture');
       if (pic) {
         const img = pic.querySelector('img');
@@ -19,7 +32,7 @@ export default function decorate(block) {
 
         const picWrapper = optimizedPic.closest('div');
         if (picWrapper && picWrapper.children.length === 1) {
-          picWrapper.classList.add('columns-img-col');
+          picWrapper.classList.add('img-container');
         }
       }
     });
