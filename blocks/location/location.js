3acd8a995dd90bba3d9a899fd057c36267e78f70
Craig McLean <craig.mclean@ensemble.com>
2026-02-06 13:13:52 -0500

    location popups

--- blocks/location/location.js
+++ blocks/location/location.js
@@ -1,5 +1,65 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
+const stateMap = {
+  alabama: 'AL',
+  alaska: 'AK',
+  arizona: 'AZ',
+  arkansas: 'AR',
+  california: 'CA',
+  colorado: 'CO',
+  connecticut: 'CT',
+  delaware: 'DE',
+  florida: 'FL',
+  georgia: 'GA',
+  hawaii: 'HI',
+  idaho: 'ID',
+  illinois: 'IL',
+  indiana: 'IN',
+  iowa: 'IA',
+  kansas: 'KS',
+  kentucky: 'KY',
+  louisiana: 'LA',
+  maine: 'ME',
+  maryland: 'MD',
+  massachusetts: 'MA',
+  michigan: 'MI',
+  minnesota: 'MN',
+  mississippi: 'MS',
+  missouri: 'MO',
+  montana: 'MT',
+  nebraska: 'NE',
+  nevada: 'NV',
+  'new hampshire': 'NH',
+  'new jersey': 'NJ',
+  'new mexico': 'NM',
+  'new york': 'NY',
+  'north carolina': 'NC',
+  'north dakota': 'ND',
+  ohio: 'OH',
+  oklahoma: 'OK',
+  oregon: 'OR',
+  pennsylvania: 'PA',
+  'rhode island': 'RI',
+  'south carolina': 'SC',
+  'south dakota': 'SD',
+  tennessee: 'TN',
+  texas: 'TX',
+  utah: 'UT',
+  vermont: 'VT',
+  virginia: 'VA',
+  washington: 'WA',
+  'west virginia': 'WV',
+  wisconsin: 'WI',
+  wyoming: 'WY',
+  ontario: 'Ontario, CA',
+};
+
+const lookupState = (name) => {
+  if (!name) return '';
+  const searchName = name.trim().toLowerCase();
+  return stateMap[searchName] || name;
+};
+
 function getMarkerPosition({ x = 0, y = 0 }) {
   const canvasWidth = 1228.6;
   const canvasHeight = 758.5;
@@ -38,7 +98,24 @@ async function renderMarkers(map, path) {
         large.classList.add('location-marker');
         const small = document.createElement('div');
         small.classList.add('location-marker-small');
-        markerDiv.append(large, small);
+
+        const popup = document.createElement('div');
+        popup.classList.add('marker-popup');
+
+        if (locationData.image) {
+          const img = document.createElement('img');
+          img.classList.add('popup-image');
+          img.src = locationData.image;
+          img.alt = locationData['data-city'] || 'Lodge Image';
+          popup.append(img);
+        }
+
+        const title = document.createElement('p');
+        title.classList.add('popup-title');
+        title.textContent = `${locationData['data-city']}, ${lookupState(locationData['data-state'])}`;
+        popup.append(title);
+
+        markerDiv.append(large, small, popup);
 
         map.append(markerDiv);
       });
