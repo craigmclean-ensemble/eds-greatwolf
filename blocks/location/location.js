5c9cfeea41c51a016fc47eb22316ebe93979657b
Craig McLean <craig.mclean@ensemble.com>
2026-02-06 11:33:13 -0500

    start of location map block, data from json, markers mapped onto map

---
+++ blocks/location/location.js
@@ -0,0 +1,68 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+function getMarkerPosition({ x = 0, y = 0 }) {
+  const canvasWidth = 1228.6;
+  const canvasHeight = 758.5;
+
+  const left = (x / canvasWidth) * 100;
+  const top = (y / canvasHeight) * 100;
+
+  return {
+    left: `${left.toFixed(4)}%`,
+    top: `${top.toFixed(4)}%`,
+  };
+}
+
+async function renderMarkers(map, path) {
+  try {
+    const resp = await fetch(path);
+
+    if (resp.ok) {
+      const json = await resp.json();
+      const markers = json.data;
+
+      markers.forEach((locationData) => {
+        const markerPosition = getMarkerPosition({ x: locationData?.['data-x'], y: locationData?.['data-y'] });
+
+        const markerDiv = document.createElement('div');
+        markerDiv.classList.add('location-marker');
+        markerDiv.style = `left: ${markerPosition.left}; top: ${markerPosition.top};`;
+
+        map.append(markerDiv);
+      });
+    }
+  } catch {
+    console.log(`Error Fetching ${path}`);
+  }
+}
+
+export default function decorate(block) {
+  const ul = document.createElement('div');
+  let dataURL = '';
+  [...block.children].forEach((row) => {
+    const li = document.createElement('div');
+
+    const link = row.querySelector('a');
+
+    if (link) {
+      dataURL = link.href;
+      row.remove();
+      return;
+    }
+
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    ul.append(li);
+  });
+
+  ul.querySelectorAll('picture > img').forEach((img) =>
+    img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '1024' }]))
+  );
+
+  const picture = ul.querySelector('picture');
+  const parentDiv = picture.closest('div');
+  parentDiv?.classList.add('location-map');
+
+  renderMarkers(parentDiv, dataURL);
+
+  block.replaceChildren(ul);
+}
