96e4bc6bc6e37ac2b99efe088124dbd6f43f3136
Craig McLean <craig.mclean@ensemble.com>
2026-02-09 12:01:04 -0500

    feature block

---
+++ blocks/features/features.js
@@ -0,0 +1,52 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+export default function decorate(block) {
+  const headerDiv = document.createElement('div');
+  headerDiv.classList.add('header');
+
+  const gridDiv = document.createElement('div');
+  gridDiv.classList.add('feature-grid');
+
+  [...block.children].forEach((row, rowIndex) => {
+    if (rowIndex === 0) {
+      const div = row.querySelector('div');
+      headerDiv.append(div);
+      return;
+    }
+
+    row
+      .querySelectorAll('picture > img')
+      .forEach((img) =>
+        img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '1024' }]))
+      );
+
+    const textContainer = row.querySelector('div:first-child');
+
+    if (textContainer) {
+      const titlesWrapper = document.createElement('div');
+      titlesWrapper.classList.add('titles-wrapper');
+
+      const p = textContainer.querySelector('p:not(.button-container)');
+      const h2 = textContainer.querySelector('h2');
+      const buttonContainer = textContainer.querySelector('.button-container');
+
+      if (p) titlesWrapper.append(p);
+      if (h2) titlesWrapper.append(h2);
+
+      textContainer.prepend(titlesWrapper);
+
+      const anchor = buttonContainer?.querySelector('a');
+      if (anchor && !anchor.querySelector('span')) {
+        const span = document.createElement('span');
+        while (anchor.firstChild) span.append(anchor.firstChild);
+        anchor.append(span);
+      }
+    }
+
+    gridDiv.append(row);
+  });
+
+  block.replaceChildren();
+
+  block.append(headerDiv, gridDiv);
+}
